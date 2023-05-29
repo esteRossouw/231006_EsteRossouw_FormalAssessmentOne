8005ff7a60aa9499c556793d0ccec5cf36164717
Este <125880105+esteRossouw@users.noreply.github.com>
2023-05-29 07:17:11 +0200

    Final try to fix javaScript
    Live reload is enabled and I can't figure out how to turn it off.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 registerStudent = () => {
-    let names = document.getElementById("names").value;
-    let email = document.getElementById("email").value;
-    let number = document.getElementById("number").value;
-    let password = document.getElementById("password").value;
-    let subject = document.getElementById("select").value;
-    let spam = document.getElementById("Check1").checked;
+  let names = document.getElementById("names").value;
+  let email = document.getElementById("email").value;
+  let number = document.getElementById("number").value;
+  let password = document.getElementById("password").value;
+  let subject = document.getElementById("select").value;
+  let spam = document.getElementById("Check1").checked;
 
-    console.log(names, email, number, password, subject, spam);
+  console.log(names, email, number, password, subject, spam);
   
-    if (spam) {
-      alert("Welcome " + names + "!" + " Thank you for your interest in " + subject + "!" + " We will contact you via the email provided: " + email + ", or alternatively on the phone number: " + number);
-    } else {
-      alert("Well, well, well... why don't you want to sign up for our newsletter... you don't seem to be that interested " +  names + "!" + " If you want to sign up, you better CHECK that newsletter box!");
-    }
+  if (spam) {
+    alert("Welcome " + names + "!" + " Thank you for your interest in " + subject + "!" + " We will contact you via the email provided: " + email + ", or alternatively on the phone number: " + number);
+  } else {
+    alert("Well, well, well... why don't you want to sign up for our newsletter... you don't seem to be that interested " +  names + "!" + " If you want to sign up, you better CHECK that newsletter box!");
+  }
 };
   
